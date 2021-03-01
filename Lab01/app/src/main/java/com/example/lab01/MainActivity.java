62f502f4379ae703f611ed0ef702ac08b8c7e825
pyb0924 <pyb0924@sjtu.edu.cn>
2021-03-01 10:41:46 +0800

    add .gitignore

---
+++ Lab01/app/src/main/java/com/example/lab01/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.lab01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+import android.widget.SearchView;
+
+public class MainActivity extends AppCompatActivity {
+    private SearchView mSearchView;
+    private AutoCompleteTextView autoCompleteTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSearchView=findViewById(R.id.sv);
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Intent intent=new Intent(MainActivity.this,SearchActivity.class);
+                startActivity(intent);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+
+    }
+}
