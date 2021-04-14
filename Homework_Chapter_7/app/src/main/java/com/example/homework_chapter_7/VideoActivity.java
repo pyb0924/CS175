53a624f0b8b7d455f597373ca8194713c7ec441d
pyb0924 <pyb0924@sjtu.edu.cn>
2021-04-14 19:29:52 +0800

    HW7 finished

--- Homework_Chapter_7/app/src/main/java/com/example/homework_chapter_7/VideoActivity.java
+++ Homework_Chapter_7/app/src/main/java/com/example/homework_chapter_7/VideoActivity.java
@@ -2,13 +2,44 @@ package com.example.homework_chapter_7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.PixelFormat;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.TestLooperManager;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 public class VideoActivity extends AppCompatActivity {
-
+    private MediaController mediaController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        final VideoView videoView=(VideoView)findViewById(R.id.video_view);
+        videoView.getHolder().setFormat(PixelFormat.TRANSPARENT);
+        videoView.setZOrderOnTop(true);
+        videoView.setVideoPath(getVideoPath(R.raw.big_buck_bunny));
+        videoView.start();
+
+        mediaController=new MediaController(this);
+        videoView.setMediaController(mediaController);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mediaController.show();
+        return super.onTouchEvent(event);
+    }
+
+    private String getVideoPath(int resId) {
+        return "android.resource://" + this.getPackageName() + "/" + resId;
     }
 }
